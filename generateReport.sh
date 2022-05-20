4c92fe26e6faa216227ebd97b745cc2518a56066
prachuryasahoo <49630788+prachuryasahoo@users.noreply.github.com>
2022-05-20 19:16:55 +1000

    Update generateReport.sh

--- generateReport.sh
+++ generateReport.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 cd reports
 
-listOfReports=(*/) 
+listreports=(*/) 
 
-listOfReports=("${arr[@]%/}")   
+listreports=("${listreports[@]%/}")   
 
-length="${#listOfReports[@]}"
+length="${#listreports[@]}"
 
-finalReport="${listOfReports[length-1]}";
+echo $length
+
+finalReport="${listreports[length-1]}";
 
 start_html='
 <!doctype html> 
@@ -19,29 +21,39 @@ start_html='
    <body>
       <nav class="uk-navbar-container" uk-navbar>
          <div class="uk-navbar-left">
-            <h2 class="uk-heading-medium">Execution Reports</h2>
+            <h2 class="uk-text-center">Execution Reports</h2>
          </div>
-         <h2 class="uk-align-right">Number of reports: <span class=uk-label> '$length'</span></h2>
       </nav>
-      <div class=uk-container uk-card-body>
-         <p>
-         <h3 class=uk-text-center> Latest execution report :
-            <a class=uk-link href="reports/'$finalReport'/report.html" />'$finalReport' </a>
-         </h3>
-         </p>
+      <div class=uk-grid>
+         <div>
+            <h3> Latest execution report :
+               <a class=uk-link href="reports/'$finalReport'/report.html" />'$finalReport' </a>
+            </h3>
+         </div>
+         <div>
+            <h3 class="uk-text-right">Number of reports: <span class=uk-label> '$length'</span></h3>
+         </div>
+         <br>
+         <br>
+         <br>
+         <br>
+         <br>
+         <hr>
       </div>
       <div class=uk-container>
+      <hr class="uk-divider-icon">
       <h2>
       Historical Reports:
       <table class="uk-table uk-table-striped">
-      <tbody>'
+      <tbody>
+'
       
 end_html='</tbody></table></div></body></html>';
 
 inner_html='';
 
-for d in "${listOfReports[@]}";do
-      innerHtml+='<tr><td><h4><a class=uk-link href="reports/'$d'/report.html">'$d'</a></h4></td></tr>'
+for d in "${listreports[@]}";do
+      inner_html+='<tr><td><h4><a class=uk-link href="reports/'$d'/report.html">'$d'</a></h4></td></tr>'
 done;
 
 cd ..
